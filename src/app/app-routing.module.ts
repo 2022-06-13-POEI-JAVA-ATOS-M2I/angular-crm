53f913d06d0a327256c40ab7cfee93c4b8cef905
dnyuhm <dylanmeuriot@gmail.com>
2022-08-26 16:27:50 +0200

    Commit 13 : Router angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageForgotPasswordComponent } from './login/pages/page-forgot-password/page-forgot-password.component';
+import { PageResetPasswordComponent } from './login/pages/page-reset-password/page-reset-password.component';
 import { PageSignInComponent } from './login/pages/page-sign-in/page-sign-in.component';
+import { PageSignUpComponent } from './login/pages/page-sign-up/page-sign-up.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: PageSignInComponent },
-  {
-    path: 'orders',
-    loadChildren: () =>
-      import('./orders/orders.module').then((m) => m.OrdersModule),
-  },
+  { path: 'sign-up', component: PageSignUpComponent },
+  { path: 'reset', component: PageResetPasswordComponent },
+  { path: 'forgot', component: PageForgotPasswordComponent },
 ];
 
 @NgModule({
