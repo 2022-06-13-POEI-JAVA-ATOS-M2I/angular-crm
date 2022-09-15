100d06d2accea5046136c1c0c82db1f84d3db11a
dnyuhm <dylanmeuriot@gmail.com>
2022-09-15 15:09:14 +0200

    end signin signup signout

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { AuthService } from 'src/app/login/services/auth.service';
+import { User } from '../../models/user';
 import { VersionService } from '../../services/version.service';
 
 @Component({
@@ -8,12 +11,23 @@ import { VersionService } from '../../services/version.service';
 })
 export class HeaderComponent implements OnInit {
   public version!: number;
+  public user$!: Subject<User | null>;
 
-  constructor(private versionService: VersionService) {
+  constructor(
+    private versionService: VersionService,
+    private authService: AuthService
+  ) {
+    this.user$ = this.authService.user$;
+
     this.versionService.numVersion.subscribe(
       (number) => (this.version = number)
     );
   }
 
   ngOnInit(): void {}
+
+  public signOut(): void {
+    this.authService.signOut();
+  }
+
 }
